ab48b3ca9938b96e3f79bb873a75582756f2aabd
messin9 <messin9@gmail.comgit config --global user.name mesin9>
2021-06-06 15:39:50 +0300

    Initial Commit

---
+++ PryanikyTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PryanikyTest
+//
+//  Created by Алексей Мотин on 06.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
